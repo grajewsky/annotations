dd66713569a51cb01ce468a643490cd9df870eb9
grajewsky <grajewski.lukasz@wp.pl>
2019-01-06 20:49:38 +0100

    Add Annotations provideR

--- src/Annotatable.php
+++ src/Annotatable.php
@@ -2,6 +2,11 @@
 
 namespace Grajewsky\Annotations\Readers;
 
-abstract class Annotatable {
-    private $annotations;
+abstract class Annotatable extends AnnotationsProvider {
+
+    public function __call($name, $args) {
+        if(\property_exists($this, $name) && !\method_exists($this, $name)) {
+            return $this->getFieldAnnotations($name);
+        }
+    }
 }
